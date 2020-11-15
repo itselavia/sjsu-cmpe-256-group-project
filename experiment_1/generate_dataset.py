e28be175bcfc23c3a19d894d4503f0e2650ea72b
Akshay Elavia <aelavia@ea.com>
2020-11-14 19:47:26 -0800

    clean the users_items file to valid JSON in each line

---
+++ experiment_1/generate_dataset.py
@@ -0,0 +1,22 @@
+import ast
+import json
+import re
+# This function will read the raw dataset file 'australian_users_items.json' and replace the single quotes to double quotes in order to make it a valid JSON
+def clean_data():
+
+    with open("/Users/akshay/Downloads/australian_users_items 2.json", "r") as raw_input:
+        with open("/Users/akshay/Downloads/australian_users_items_cleaned.json", "w") as cleaned_file:
+            for line in raw_input:
+                withoutSingleQuotes = line.replace('\'', '\"')
+                main_item_name_indexes = [m.start() for m in re.finditer('item_name', withoutSingleQuotes)]
+                for main_item_name_index in main_item_name_indexes:
+                    main_item_name_index = main_item_name_index + 13
+                    current_play_index = withoutSingleQuotes[main_item_name_index:].find("playtime_forever")
+                    temp = withoutSingleQuotes[main_item_name_index: main_item_name_index + current_play_index - 4 ]
+                    repeatingDoubleQuotesIndexes = [m.start() for m in re.finditer("\"", temp)]
+                    for i in repeatingDoubleQuotesIndexes:
+                        toReplaceIndex = main_item_name_index + i
+                        withoutSingleQuotes = withoutSingleQuotes[:toReplaceIndex] + "'" + withoutSingleQuotes[toReplaceIndex + 1:]
+                cleaned_file.write(withoutSingleQuotes)
+if __name__ == "__main__":
+    clean_data()
